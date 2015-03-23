41e1084654d8986974fa7a79d08dd04b871dadb0
André Bires <andrebires@gmail.com>
2015-03-22 22:17:25 -0300

    Added async LINQ extensions.

---
+++ src/Takenet.SimplePersistence/IAsyncEnumerable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Takenet.SimplePersistence
+{
+    public interface IAsyncEnumerable<T> : IEnumerable<T>
+    {
+        Task<IAsyncEnumerator<T>> GetEnumeratorAsync(CancellationToken cancellationToken);
+    }
+
+    public interface IAsyncEnumerable : IEnumerable
+    {
+        Task<IAsyncEnumerator> GetEnumeratorAsync(CancellationToken cancellationToken);
+    }
+}
--- src/Takenet.SimplePersistence/IAsyncEnumerator.cs
+++ src/Takenet.SimplePersistence/IAsyncEnumerator.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Takenet.SimplePersistence
@@ -9,16 +11,13 @@ namespace Takenet.SimplePersistence
     /// <summary>
     /// Defines a generic asynchronous iterator.
     /// </summary>
-    public interface IAsyncEnumerator<out T>
+    public interface IAsyncEnumerator<out T> : IAsyncEnumerator, IEnumerator<T>
     {
-        /// <summary>
-        /// Gets the element in the collection at the current position of the enumerator.
-        /// </summary>
-        /// <returns>
-        /// The element in the collection at the current position of the enumerator.
-        /// </returns>
-        T Current { get; }
 
+    }
+
+    public interface IAsyncEnumerator : IEnumerator, IDisposable
+    {
         /// <summary>
         /// Advances the enumerator to the next element of the collection.
         /// </summary>
@@ -26,12 +25,6 @@ namespace Takenet.SimplePersistence
         /// true if the enumerator was successfully advanced to the next element; false if the enumerator has passed the end of the collection.
         /// </returns>
         /// <exception cref="T:System.InvalidOperationException">The collection was modified after the enumerator was created. </exception><filterpriority>2</filterpriority>
-        Task<bool> MoveNextAsync();
-
-        /// <summary>
-        /// Sets the enumerator to its initial position, which is before the first element in the collection.
-        /// </summary>
-        /// <exception cref="T:System.InvalidOperationException">The collection was modified after the enumerator was created. </exception><filterpriority>2</filterpriority>
-        Task ResetAsync();
+        Task<bool> MoveNextAsync(CancellationToken cancellationToken);
     }
 }
