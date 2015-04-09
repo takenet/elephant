59ddad3f675c0eef2554113e9a5ac61aa3f570c1
André Bires <andrebires@gmail.com>
2015-04-08 22:14:38 -0300

    Additional RedisSetMap tests.

--- src/Takenet.SimplePersistence.Tests/Redis/RedisIntegerStringQueueMapFacts.cs
+++ src/Takenet.SimplePersistence.Tests/Redis/RedisIntegerStringQueueMapFacts.cs
@@ -12,30 +12,30 @@ using Xunit;
 namespace Takenet.SimplePersistence.Tests.Redis
 {
     [Collection("Redis")]
-    public class RedisIntegerStringSetMapFacts : IntegerStringSetMapFacts
+    public class RedisIntegerStringQueueMapFacts : IntegerStringQueueMapFacts
     {
         private readonly RedisFixture _redisFixture;
         public const string MapName = "integer-strings";
 
-        public RedisIntegerStringSetMapFacts(RedisFixture redisFixture)
+        public RedisIntegerStringQueueMapFacts(RedisFixture redisFixture)
         {
             _redisFixture = redisFixture;
         }
 
-        public override IMap<int, ISet<string>> Create()
+        public override IMap<int, IQueue<string>> Create()
         {            
             _redisFixture.Server.FlushDatabase();            
-            var setMap = new RedisSetMap<int, string>(MapName, _redisFixture.Connection.Configuration, new StringSerializer());
+            var setMap = new RedisQueueMap<int, string>(MapName, _redisFixture.Connection.Configuration, new StringSerializer());
             return setMap;
         }
 
-        public override ISet<string> CreateValue(int key)
+        public override IQueue<string> CreateValue(int key)
         {
-            var set = new Set<string>();
-            set.AddAsync(Fixture.Create<string>()).Wait();
-            set.AddAsync(Fixture.Create<string>()).Wait();
-            set.AddAsync(Fixture.Create<string>()).Wait();
-            return set;
+            var queue = new Queue<string>();
+            queue.EnqueueAsync(Fixture.Create<string>()).Wait();
+            queue.EnqueueAsync(Fixture.Create<string>()).Wait();
+            queue.EnqueueAsync(Fixture.Create<string>()).Wait();
+            return queue;
         }
     }
 }
