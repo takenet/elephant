ac22d8a8f2eb3c10415302da239459b90704e6f2
André Bires <andrebires@gmail.com>
2016-02-25 15:00:14 -0300

    Added ScopedQueueMap.

---
+++ src/Takenet.Elephant.Specialized/Scoping/ScopedQueueMap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Takenet.Elephant.Specialized.Scoping
+{
+    public class ScopedQueueMap<TKey, TValue> : ScopedMap<TKey, IQueue<TValue>>, IQueueMap<TKey, TValue>
+    {
+        public ScopedQueueMap(IMap<TKey, IQueue<TValue>> map, IScope scope, ISerializer<TKey> keySerializer) 
+            : base(map, scope, keySerializer)
+        {
+        }
+    }
+}
