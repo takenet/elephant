55dc67d61c8b425725f51ab5e892e82b336c051b
André Bires <andrebires@gmail.com>
2016-05-13 10:41:38 -0300

    Changed SqlHelper visiblity to public

--- src/Takenet.Elephant.Sql/SqlHelper.cs
+++ src/Takenet.Elephant.Sql/SqlHelper.cs
@@ -7,42 +7,42 @@ using System.Text;
 
 namespace Takenet.Elephant.Sql
 {
-    internal static class SqlHelper
+    public static class SqlHelper
     {        
-        internal static string GetAndEqualsStatement(IDatabaseDriver databaseDriver, IDictionary<string, object> filterValues)
+        public static string GetAndEqualsStatement(IDatabaseDriver databaseDriver, IDictionary<string, object> filterValues)
         {
             if (filterValues == null) return databaseDriver.GetSqlStatementTemplate(SqlStatement.OneEqualsOne);
             return GetAndEqualsStatement(databaseDriver, filterValues.Keys.ToArray());
         }
 
-        internal static string GetAndEqualsStatement(IDatabaseDriver databaseDriver, string[] columns)
+        public static string GetAndEqualsStatement(IDatabaseDriver databaseDriver, string[] columns)
         {
             return GetAndEqualsStatement(databaseDriver, columns, columns);
         }
 
-        internal static string GetAndEqualsStatement(IDatabaseDriver databaseDriver, string[] columns, string[] parameters)
+        public static string GetAndEqualsStatement(IDatabaseDriver databaseDriver, string[] columns, string[] parameters)
         {
             return columns.Length == 0 ?
                 databaseDriver.GetSqlStatementTemplate(SqlStatement.OneEqualsOne) : 
                 GetSeparateEqualsStatement(databaseDriver, databaseDriver.GetSqlStatementTemplate(SqlStatement.And), columns, parameters);
         }
 
-        internal static string GetCommaEqualsStatement(IDatabaseDriver databaseDriver, string[] columns)
+        public static string GetCommaEqualsStatement(IDatabaseDriver databaseDriver, string[] columns)
         {
             return GetCommaEqualsStatement(databaseDriver, columns, columns);
         }
 
-        internal static string GetCommaEqualsStatement(IDatabaseDriver databaseDriver, string[] columns, string[] parameters)
+        public static string GetCommaEqualsStatement(IDatabaseDriver databaseDriver, string[] columns, string[] parameters)
         {
             return GetSeparateEqualsStatement(databaseDriver, ",", columns, parameters);
         }
 
-        internal static string GetSeparateEqualsStatement(IDatabaseDriver databaseDriver, string separator, string[] columns, string[] parameters)
+        public static string GetSeparateEqualsStatement(IDatabaseDriver databaseDriver, string separator, string[] columns, string[] parameters)
         {
             return GetSeparateColumnsStatement(databaseDriver, separator, columns, parameters, GetEqualsStatement);
         }
 
-        internal static string GetEqualsStatement(IDatabaseDriver databaseDriver, string column, string parameter)
+        public static string GetEqualsStatement(IDatabaseDriver databaseDriver, string column, string parameter)
         {
             return databaseDriver.GetSqlStatementTemplate(SqlStatement.QueryEquals).Format(
                 new
@@ -52,22 +52,22 @@ namespace Takenet.Elephant.Sql
                 });
         }
 
-        internal static string GetCommaValueAsColumnStatement(IDatabaseDriver databaseDriver, string[] columns)
+        public static string GetCommaValueAsColumnStatement(IDatabaseDriver databaseDriver, string[] columns)
         {
             return GetValueAsColumnStatement(databaseDriver, ",", columns);
         }
 
-        internal static string GetValueAsColumnStatement(IDatabaseDriver databaseDriver, string separator, string[] columns)
+        public static string GetValueAsColumnStatement(IDatabaseDriver databaseDriver, string separator, string[] columns)
         {
             return GetValueAsColumnStatement(databaseDriver, separator, columns, columns);
         }
 
-        internal static string GetValueAsColumnStatement(IDatabaseDriver databaseDriver, string separator, string[] columns, string[] parameters)
+        public static string GetValueAsColumnStatement(IDatabaseDriver databaseDriver, string separator, string[] columns, string[] parameters)
         {
             return GetSeparateColumnsStatement(databaseDriver, separator, columns, parameters, GetValueAsColumnStatement);
         }
 
-        internal static string GetValueAsColumnStatement(IDatabaseDriver databaseDriver, string column, string parameter)
+        public static string GetValueAsColumnStatement(IDatabaseDriver databaseDriver, string column, string parameter)
         {
             return databaseDriver.GetSqlStatementTemplate(SqlStatement.ValueAsColumn).Format(
                 new
@@ -77,7 +77,7 @@ namespace Takenet.Elephant.Sql
                 });
         }
 
-        internal static string GetLiteralJoinConditionStatement(IDatabaseDriver databaseDriver, string[] columns, string sourceTableName, string targetTableName)
+        public static string GetLiteralJoinConditionStatement(IDatabaseDriver databaseDriver, string[] columns, string sourceTableName, string targetTableName)
         {
             return GetSeparateColumnsStatement(
                 databaseDriver,
@@ -93,7 +93,7 @@ namespace Takenet.Elephant.Sql
 
         }
 
-        internal static string GetSeparateColumnsStatement(IDatabaseDriver databaseDriver, string separator, string[] columns, string[] parameters, Func<IDatabaseDriver, string, string, string> statement)
+        public static string GetSeparateColumnsStatement(IDatabaseDriver databaseDriver, string separator, string[] columns, string[] parameters, Func<IDatabaseDriver, string, string, string> statement)
         {
             if (columns.Length == 0)
             {
@@ -124,7 +124,7 @@ namespace Takenet.Elephant.Sql
             return filter.ToString();
         }
 
-        internal static string TranslateToSqlWhereClause<TEntity>(IDatabaseDriver databaseDriver, Expression<Func<TEntity, bool>> where, IDictionary<string, string> parameterReplacementDictionary = null)
+        public static string TranslateToSqlWhereClause<TEntity>(IDatabaseDriver databaseDriver, Expression<Func<TEntity, bool>> where, IDictionary<string, string> parameterReplacementDictionary = null)
         {            
             if (where == null) return databaseDriver.GetSqlStatementTemplate(SqlStatement.OneEqualsOne);
             var translator = new SqlExpressionTranslator(databaseDriver, parameterReplacementDictionary);
