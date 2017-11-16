57ac4e994c4517640859d0bc724a07d06ca1df61
André Bires Fonseca <andrebires@gmail.com>
2017-11-16 11:27:53 -0200

    Added an extension method for dequeueing items directly from a IQueueMap

--- src/Takenet.Elephant/QueueMapExtensions.cs
+++ src/Takenet.Elephant/QueueMapExtensions.cs
@@ -20,5 +20,21 @@ namespace Takenet.Elephant
             var queue = await queueMap.GetValueOrEmptyAsync(key).ConfigureAwait(false);
             await queue.EnqueueAsync(item).ConfigureAwait(false);
         }
+
+        /// <summary>
+        /// Dequeue an item in the queue on the specified key.
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TItem"></typeparam>
+        /// <param name="queueMap"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static async Task<TItem> DequeueItemOrDefaultAsync<TKey, TItem>(this IQueueMap<TKey, TItem> queueMap,
+            TKey key)
+        {
+            var queue = await queueMap.GetValueOrDefaultAsync(key).ConfigureAwait(false);
+            if (queue == null) return default(TItem);
+            return await queue.DequeueOrDefaultAsync().ConfigureAwait(false);
+        }
     }
 }
