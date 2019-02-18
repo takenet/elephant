b0a4d462990108a3f5621ce1761ad1e36746ca82
Andre Minelli <andre@take.net>
2019-02-18 09:12:51 -0300

    Add tests for SQL Server Set with an Identity column

---
+++ src/Take.Elephant.Tests/Sql/SqlItemSetWithIdentityFacts.cs
@@ -0,0 +1,27 @@
+﻿using Take.Elephant.Sql;
+using Take.Elephant.Sql.Mapping;
+
+namespace Take.Elephant.Tests.Sql
+{
+    public abstract class SqlItemSetWithIdentityFacts : ItemSetFacts
+    {
+        private readonly ISqlFixture _serverFixture;
+
+        protected SqlItemSetWithIdentityFacts(ISqlFixture serverFixture)
+        {
+            _serverFixture = serverFixture;
+        }
+
+        public override ISet<Item> Create()
+        {
+            var table = TableBuilder
+                .WithName("ItemsSetWithIdentity")
+                .WithKeyColumnFromType<int>(nameof(Item.IntegerProperty), isIdentity: true)
+                .WithColumnsFromTypeProperties<Item>(/*p => !p.Name.Equals(nameof(Item.IntegerProperty))*/)
+                .Build();
+            _serverFixture.DropTable(table.Schema, table.Name);
+            var mapper = new TypeMapper<Item>(table);
+            return new SqlSet<Item>(_serverFixture.DatabaseDriver, _serverFixture.ConnectionString, table, mapper);
+        }
+    }
+}
