8390ed86efaeaf602d3310b4092fddc5212e7cd7
Felipe Nader <felipen@take.net>
2019-08-06 10:48:56 -0300

    Downgrade to NEST 6.8.2

--- src/Take.Elephant.ElasticSearch/Mapping/MappingBuilder.cs
+++ src/Take.Elephant.ElasticSearch/Mapping/MappingBuilder.cs
@@ -33,6 +33,11 @@ namespace Take.Elephant.Elasticsearch.Mapping
         public string KeyField { get; set; }
 
         /// <summary>
+        /// Gets the key field
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
         /// Includes the document index name
         /// </summary>
         /// <param name="index"></param>
@@ -43,12 +48,23 @@ namespace Take.Elephant.Elasticsearch.Mapping
         }
 
         /// <summary>
+        /// Includes the document type name
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public MappingBuilder WithType(string type)
+        {
+            Type = type;
+            return this;
+        }
+
+        /// <summary>
         /// Creates an IMapping instance based on the provided parameters
         /// </summary>
         /// <returns></returns>
         public IMapping Build()
         {
-            return new Mapping(Index, KeyField);
+            return new Mapping(Index, Type, KeyField);
         }
     }
 }
