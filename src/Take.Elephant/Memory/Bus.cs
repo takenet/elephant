fff4abe601a201d57f8fa6fabef839fd0e8b90f0
André Bires <andrebires@gmail.com>
2020-04-15 17:07:00 -0300

    Changed memory bus handler store strategy

--- src/Take.Elephant/Memory/Bus.cs
+++ src/Take.Elephant/Memory/Bus.cs
@@ -13,27 +13,22 @@ namespace Take.Elephant.Memory
     /// <typeparam name="TMessage"></typeparam>
     public class Bus<TChannel, TMessage> : IBus<TChannel, TMessage>
     {
-        private readonly string _busName;
-        private static readonly ConcurrentDictionary<string, ConcurrentDictionary<TChannel, ConcurrentBag<Func<TChannel, TMessage, CancellationToken, Task>>>> _busChannelHandlerDictionary;
+        private readonly BusHandlerStore<TChannel, TMessage> _handlerStore;
 
-        static Bus()
+        public Bus()
+            : this(BusHandlerStore<TChannel, TMessage>.Default)
         {
-            _busChannelHandlerDictionary = new ConcurrentDictionary<string, ConcurrentDictionary<TChannel, ConcurrentBag<Func<TChannel, TMessage, CancellationToken, Task>>>>();
+            
+        }
+
+        public Bus(BusHandlerStore<TChannel, TMessage> handlerStore)
+        {
+            _handlerStore = handlerStore ?? throw new ArgumentNullException(nameof(handlerStore));
         }
         
-        public Bus(string busName = "default")
-        {
-            _busName = busName ?? throw new ArgumentNullException(nameof(busName));
-        }
-
-        private ConcurrentDictionary<TChannel, ConcurrentBag<Func<TChannel, TMessage, CancellationToken, Task>>> ChannelHandlerDictionary =>
-            _busChannelHandlerDictionary.GetOrAdd(
-                _busName, 
-                _ => new ConcurrentDictionary<TChannel, ConcurrentBag<Func<TChannel, TMessage, CancellationToken, Task>>>());
-
         public virtual Task SubscribeAsync(TChannel channel, Func<TChannel, TMessage, CancellationToken, Task> handler, CancellationToken cancellationToken)
         {
-            ChannelHandlerDictionary.AddOrUpdate(
+            _handlerStore.ChannelHandlerDictionary.AddOrUpdate(
                 channel,
                 new ConcurrentBag<Func<TChannel, TMessage, CancellationToken, Task>>()
                 {
@@ -50,15 +45,32 @@ namespace Take.Elephant.Memory
 
         public virtual Task UnsubscribeAsync(TChannel channel, CancellationToken cancellationToken)
         {
-            ChannelHandlerDictionary.TryRemove(channel, out _);
+            _handlerStore.ChannelHandlerDictionary.TryRemove(channel, out _);
             return Task.CompletedTask;
         }
 
         public virtual async Task PublishAsync(TChannel channel, TMessage message, CancellationToken cancellationToken)
         {
-            if (!ChannelHandlerDictionary.TryGetValue(channel, out var handlers)) return;
+            if (!_handlerStore.ChannelHandlerDictionary.TryGetValue(channel, out var handlers)) return;
 
             await Task.WhenAll(handlers.Select(h => h(channel, message, cancellationToken)));
         }
+    }
+
+    /// <summary>
+    /// Implements a store for <see cref="Bus{TChannel,TMessage}"/> handlers.
+    /// </summary>
+    /// <typeparam name="TChannel"></typeparam>
+    /// <typeparam name="TMessage"></typeparam>
+    public sealed class BusHandlerStore<TChannel, TMessage>
+    {
+        public static BusHandlerStore<TChannel, TMessage> Default { get; } = new BusHandlerStore<TChannel, TMessage>();
+        
+        public BusHandlerStore()
+        {
+            ChannelHandlerDictionary = new ConcurrentDictionary<TChannel, ConcurrentBag<Func<TChannel, TMessage, CancellationToken, Task>>>();
+        }
+        
+        public ConcurrentDictionary<TChannel, ConcurrentBag<Func<TChannel, TMessage, CancellationToken, Task>>> ChannelHandlerDictionary { get; }
     }
 }
