2a404056007d0b2a05afdda4edfe4a1f5e24a356
André Silva de Jesus <andre_ss6@hotmail.com>
2021-09-24 18:32:51 -0300

    Fix Kafka unit tests that weren't disposing the queues

--- src/Take.Elephant.Tests/Kafka/KafkaItemSenderReceiverQueueFacts.cs
+++ src/Take.Elephant.Tests/Kafka/KafkaItemSenderReceiverQueueFacts.cs
@@ -10,9 +10,11 @@ namespace Take.Elephant.Tests.Kafka
     [Trait("Category", nameof(Kafka))]
     public class KafkaItemSenderReceiverQueueFacts : ItemSenderReceiverQueueFacts
     {
-        private readonly string topic = "items";
-        private static readonly ClientConfig clientConfig = GetKafkaConfig();
-        private static readonly ConsumerConfig consumerConfig = new ConsumerConfig(clientConfig) { GroupId = "default" };
+        private readonly string _topic = "items";
+        private static readonly ClientConfig _clientConfig = GetKafkaConfig();
+        private static readonly ConsumerConfig _consumerConfig = new ConsumerConfig(_clientConfig) { GroupId = "default" };
+        private KafkaSenderQueue<Item> _senderQueue;
+        private KafkaReceiverQueue<Item> _receiverQueue;
 
         private static ClientConfig GetKafkaConfig()
         {
@@ -49,14 +51,14 @@ namespace Take.Elephant.Tests.Kafka
 
         public override (ISenderQueue<Item>, IBlockingReceiverQueue<Item>) Create()
         {
-            var consumerConfig = new ConsumerConfig(clientConfig)
+            var consumerConfig = new ConsumerConfig(_clientConfig)
             {
                 GroupId = "default"
             };
             
-            var adminClient = new AdminClientBuilder(clientConfig).Build();
+            var adminClient = new AdminClientBuilder(_clientConfig).Build();
             var metadata = adminClient.GetMetadata(TimeSpan.FromSeconds(5));
-            var topicMetadata = metadata.Topics.FirstOrDefault(t => t.Topic == topic);
+            var topicMetadata = metadata.Topics.FirstOrDefault(t => t.Topic == _topic);
             if (topicMetadata != null)
             {
                 var consumer = new ConsumerBuilder<Ignore, Item>(consumerConfig)
@@ -65,17 +67,28 @@ namespace Take.Elephant.Tests.Kafka
 
                 foreach (var partition in topicMetadata.Partitions)
                 {
-                    var topicPartition = new TopicPartition(topic, new Partition(partition.PartitionId));
+                    var topicPartition = new TopicPartition(_topic, new Partition(partition.PartitionId));
                     var offSet = consumer.QueryWatermarkOffsets(topicPartition, TimeSpan.FromSeconds(5));
                     consumer.Commit(new TopicPartitionOffset[] {new TopicPartitionOffset(topicPartition, offSet.High)});
                 }
                 consumer.Close();
             }
             
-            var senderQueue = new KafkaSenderQueue<Item>(new ProducerConfig(clientConfig), topic, new JsonItemSerializer());
-            var receiverQueue = new KafkaReceiverQueue<Item>(consumerConfig, topic, new JsonItemSerializer());
+            var senderQueue = new KafkaSenderQueue<Item>(new ProducerConfig(_clientConfig), _topic, new JsonItemSerializer());
+            var receiverQueue = new KafkaReceiverQueue<Item>(consumerConfig, _topic, new JsonItemSerializer());
             receiverQueue.OpenAsync(CancellationToken).Wait();
+
+            _senderQueue = senderQueue;
+            _receiverQueue = receiverQueue;
+
             return (senderQueue, receiverQueue);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _senderQueue?.Dispose();
+            _receiverQueue?.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
