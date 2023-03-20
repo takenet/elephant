d09f4276c2d1ec87dbe6791a853b06e77a377ed8
Victor Guimarães <victor@stilingue.com.br>
2023-03-20 18:12:32 -0300

    fix: Fix parameter separator to be Postgr friendly

--- src/Take.Elephant.Sql/SqlExpressionTranslator.cs
+++ src/Take.Elephant.Sql/SqlExpressionTranslator.cs
@@ -11,7 +11,7 @@ namespace Take.Elephant.Sql
 {
     public class SqlExpressionTranslator : ExpressionVisitor
     {
-        internal const string PARAMETER_COUNT_SEPARATOR = "$";
+        internal const string PARAMETER_COUNT_SEPARATOR = "__";
 
         private readonly StringBuilder _filter;
         private readonly IDictionary<string, object> _filterValues;
@@ -304,7 +304,7 @@ namespace Take.Elephant.Sql
                 name = _parameterNames.Dequeue();
                 if (_filterValues.ContainsKey(name))
                 {
-                    name = $"{name}{PARAMETER_COUNT_SEPARATOR}{_parameterCount}";
+                    name = $"{name}{PARAMETER_COUNT_SEPARATOR}{_parameterCount}{PARAMETER_COUNT_SEPARATOR}";
                 }
             }
 
